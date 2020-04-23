643df65cd73fd38e7329871b3b4378a67fba86fd
HimanshuVarun <himanshuvarun9756@gmail.com>
2020-04-23 21:09:05 +0530

    first commit

---
+++ src/test/java/MavenWithAll/MavenWithAll/Hello5.java
@@ -0,0 +1,21 @@
+package MavenWithAll.MavenWithAll;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class Hello5
+{
+	@BeforeTest
+	public void first()
+	{
+		System.out.println("Hello innnnnnnnnnnnnnnnnnnnnnnnnnnn MavenWithAll");
+	}
+	@Test
+	public void last()
+	{
+		System.out.println("Hiiii innnnnnnnnnnnnnnnnnnnnnnnnnnn MavenWithAll");
+
+	}
+
+}
